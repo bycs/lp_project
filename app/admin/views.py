94b3e0817de7643674427d89f54456d3168d8f64
Stas Cartoon <bycs@mail.ru>
2022-03-30 18:58:00 +0300

    Добавление декоратора @admin_required

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,15 +1,12 @@
 from flask import Blueprint
 
-from flask_login import current_user, login_required
-
+from app.admin.decorators import admin_required
 
 blueprint = Blueprint("admin", __name__, url_prefix="/admin")
 
 
 @blueprint.route("/")
-@login_required
+@admin_required
 def admin_page():
-    if current_user.is_admin:
-        return "Добро пожаловать, Админ"
-    else:
-        return "Ты не админ!"
+    return "Добро пожаловать, Админ"
+
