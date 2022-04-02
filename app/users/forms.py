1645eca8d3fe010175812af273e82553b8c8d86a
Stas Cartoon <bycs@mail.ru>
2022-04-02 17:39:21 +0300

    Добавлена валидация e-mail

--- app/users/forms.py
+++ app/users/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo
 
 
 class RegisterForm(FlaskForm):
-    email = StringField("e-mail", validators=[DataRequired()])
+    email = StringField("e-mail", validators=[DataRequired(), Email()])
     password = PasswordField("Пароль", validators=[DataRequired()])
     password2 = PasswordField(
         "Повтор пароля", validators=[DataRequired(), EqualTo("password")]
@@ -17,7 +17,7 @@ class RegisterForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = StringField("e-mail", validators=[DataRequired()])
+    email = StringField("e-mail", validators=[DataRequired(), Email()])
     password = PasswordField("Пароль", validators=[DataRequired()])
     remember_user = BooleanField(
         "Запомнить меня", default=True, render_kw={"class": "form-check-input"}
