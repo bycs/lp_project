4d15b7ece15a97727d0d4f90d19189a29c14f69e
Stas Cartoon <bycs@mail.ru>
2022-04-09 00:37:19 +0300

    Просмотр пользователей
    Добавлен просмотр пользователей

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,6 +1,7 @@
 from app.admin.decorators import admin_required
+from app.admin.utils import get_all_users
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 blueprint = Blueprint("admin", __name__, url_prefix="/admin")
 
@@ -9,3 +10,11 @@ blueprint = Blueprint("admin", __name__, url_prefix="/admin")
 @admin_required
 def admin_page():
     return "Добро пожаловать, Админ"
+
+
+@blueprint.route("/users")
+@admin_required
+def users_list():
+    users = get_all_users()
+    title = "Пользователи"
+    return render_template("admin/users.html", page_title=title, users=users)
