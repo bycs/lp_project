f097e88ad552dc418e63bb4723bafb27a85c590b
Stas Cartoon <bycs@mail.ru>
2022-04-09 01:14:16 +0300

    fix register
    Исправлен баг с пустой строкой в отчестве

--- app/user/utils.py
+++ app/user/utils.py
@@ -3,10 +3,18 @@ from app.user.models import User
 
 
 def create_user(form):
+    try:
+        if len(form.middle_name) < 1:
+            form.middle_name = None
+        else:
+            middle_name = form.middle_name
+    except TypeError:
+        middle_name = None
+
     new_user = User(
         email=form.email.data,
         first_name=form.first_name.data,
-        middle_name=form.middle_name.data,
+        middle_name=middle_name,
         last_name=form.last_name.data,
     )
 
