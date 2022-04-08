b1f029a001692735aed08cc8c70752747b6ecafe
Stas Cartoon <bycs@mail.ru>
2022-04-09 00:35:51 +0300

    Добавление фронта
    Обвновилсь шаблоны, добавлены стили, добавлено меню

--- app/user/forms.py
+++ app/user/forms.py
@@ -5,21 +5,39 @@ from wtforms.validators import DataRequired, Email, EqualTo
 
 
 class RegisterForm(FlaskForm):
-    email = StringField("e-mail", validators=[DataRequired(), Email()])
-    password = PasswordField("Пароль", validators=[DataRequired()])
-    password2 = PasswordField(
-        "Повтор пароля", validators=[DataRequired(), EqualTo("password")]
+    email = StringField(
+        "Email",
+        validators=[DataRequired(), Email()],
+        render_kw={"class": "form-control"},
     )
-    first_name = StringField("Имя", validators=[DataRequired()])
-    middle_name = StringField("Отчество")
-    last_name = StringField("Фамилия", validators=[DataRequired()])
-    submit = SubmitField("Зарегистрироваться")
+    password = PasswordField(
+        "Пароль", validators=[DataRequired()], render_kw={"class": "form-control"}
+    )
+    password2 = PasswordField(
+        "Повтор пароля",
+        validators=[DataRequired(), EqualTo("password")],
+        render_kw={"class": "form-control"},
+    )
+    first_name = StringField(
+        "Имя", validators=[DataRequired()], render_kw={"class": "form-control"}
+    )
+    middle_name = StringField("Отчество", render_kw={"class": "form-control"})
+    last_name = StringField(
+        "Фамилия", validators=[DataRequired()], render_kw={"class": "form-control"}
+    )
+    submit = SubmitField("Зарегистрироваться", render_kw={"class": "btn btn-primary"})
 
 
 class LoginForm(FlaskForm):
-    email = StringField("e-mail", validators=[DataRequired(), Email()])
-    password = PasswordField("Пароль", validators=[DataRequired()])
+    email = StringField(
+        label="Email",
+        validators=[DataRequired(), Email()],
+        render_kw={"class": "form-control"},
+    )
+    password = PasswordField(
+        label="Пароль", validators=[DataRequired()], render_kw={"class": "form-control"}
+    )
     remember_user = BooleanField(
         "Запомнить меня", default=True, render_kw={"class": "form-check-input"}
     )
-    submit = SubmitField("Войти")
+    submit = SubmitField("Войти", render_kw={"class": "btn btn-primary"})
